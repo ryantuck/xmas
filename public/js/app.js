abf0174a66c1be8d5d3d5f1436b15fa28a969123
ryantuck <ryantuck@icloud.com>
2014-11-04 14:09:56 -0500

    clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('sampleApp', 
+	['ngRoute',
+  'appRoutes',
+  'MainCtrl',
+  'NerdCtrl',
+  'NerdService',
+  'GeekCtrl',
+  'GeekService'
+]);
