5db5cec5ac3463cd45ca5fe796773201f63c7e93
ryantuck <ryantuck@icloud.com>
2014-11-05 12:24:41 -0500

    generate list of default presents

--- public/js/controllers/PresentCtrl.js
+++ public/js/controllers/PresentCtrl.js
@@ -1,7 +1,20 @@
+// PresentCtrl.js
+
 angular.module('PresentCtrl',[]).controller('PresentController',function($scope){
 
 	$scope.tagline = 'i want presents!';
 	$scope.intro = 'hey dude get some prezzies.';
-	$scope.numPresents = 42;
+
+	// Default set of presents
+	$scope.presents = [
+		{title: 'moleskine', priority: 1},
+		{title: 'chef knife', priority: 3},
+		{title: 'unders', priority: 2},
+	];
+
+	$scope.getTotalPresents = function() {
+		return $scope.presents.length;
+	};
+
 
 });
