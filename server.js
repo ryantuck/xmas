e0193cf9793cf2758edb4fc920f720733ff972c9
ryantuck <ryantuck@icloud.com>
2014-11-10 15:26:46 -0500

    add passport stuff to server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+// server.js
+
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 // configuration ===========================================
-	
+
 // config files
 var db = require('./config/db');
 mongoose.connect(db.url);
@@ -16,10 +24,31 @@ var Bear = require('./app/models/bear');
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
+require('./config/passport')(passport) // pass in passport for configuration
+
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // parse application/x-www-form-urlencoded
+
+app.use(morgan('dev'));
+app.use(cookieParser()); // read cookies (needed for auth)
+
+// passport stuff
+app.use(session({
+  secret: 'wildwildwest',
+  saveUninitialized: true,
+  resave: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // for flash messages stored in session
+
+
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
@@ -28,86 +57,86 @@ app.use(express.static(__dirname + '/public')); // set the static files location
 var router = express.Router();
 
 // middleware to use for all requests
-router.use(function(req,res,next){
-	// log something
-	console.log('something is happening!');
-	next(); // goes to next route (doesn't stop here)
+router.use(function(req, res, next) {
+  // log something
+  console.log('something is happening!');
+  next(); // goes to next route (doesn't stop here)
 });
 
 // tests route to make sure things are working
-router.get('/',function(req,res){
-	res.json({message: 'hey welcome to the api'});
+router.get('/', function(req, res) {
+  res.json({
+    message: 'hey welcome to the api'
+  });
 });
 
 router.route('/bears')
-	
-	// create a bear
-	.post(function(req,res){
-		var bear = new Bear();
-		bear.name = req.body.name;
 
-		bear.save(function(err){
-			if (err) res.send(err);
-			res.json({message:'bear created!'});
-		});
-	})
+// create a bear
+.post(function(req, res) {
+  var bear = new Bear();
+  bear.name = req.body.name;
 
-	// get all bears
-	.get(function(req,res){
-		Bear.find(function(err, bears){
-			if (err) res.send(err);
-			res.json(bears);
-		});
-	});
+  bear.save(function(err) {
+    if (err) res.send(err);
+    res.json({
+      message: 'bear created!'
+    });
+  });
+})
+
+// get all bears
+.get(function(req, res) {
+  Bear.find(function(err, bears) {
+    if (err) res.send(err);
+    res.json(bears);
+  });
+});
 
 router.route('/bears/:bear_id')
 
-	// get a bear with a certain id
-	.get(function(req,res){
-		Bear.findById(req.params.bear_id, function(err,bear){
-			if (err) res.send(err);
-			res.json(bear);
-		});
-	})
+// get a bear with a certain id
+.get(function(req, res) {
+  Bear.findById(req.params.bear_id, function(err, bear) {
+    if (err) res.send(err);
+    res.json(bear);
+  });
+})
 
-	.put(function(req,res){
-		Bear.findById(req.params.bear_id, function(err,bear){
-			if (err) res.send(err);
-			bear.name = req.body.name;
-			bear.save(function(err){
-				if (err) res.send(err);
-				res.json({message:'bear updated!'});
-			});
-		});
-	})
+.put(function(req, res) {
+  Bear.findById(req.params.bear_id, function(err, bear) {
+    if (err) res.send(err);
+    bear.name = req.body.name;
+    bear.save(function(err) {
+      if (err) res.send(err);
+      res.json({
+        message: 'bear updated!'
+      });
+    });
+  });
+})
 
-	.delete(function(req,res){
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err,bear){
-			if (err) res.send(err);
-			res.json({message: 'bear successfully deleted'});
-		});
-	});
+.delete(function(req, res) {
+  Bear.remove({
+    _id: req.params.bear_id
+  }, function(err, bear) {
+    if (err) res.send(err);
+    res.json({
+      message: 'bear successfully deleted'
+    });
+  });
+});
 
 // registers routes
-app.use('/api',router);
+app.use('/api', router);
 
 
 // routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+require('./app/routes')(app, passport); // pass our application into our routes
 
 
 
 // start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
-
-
-
-
-
-
-
-
+app.listen(port);
+console.log('Magic happens on port ' + port); // shoutout to the user
+exports = module.exports = app; // expose app
