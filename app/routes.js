879a6e8972d90c47f252287b26faab449e3ddccf
ryantuck <ryantuck@icloud.com>
2014-12-10 10:09:02 -0500

    just throwing shit at the wall

--- app/routes.js
+++ app/routes.js
@@ -9,13 +9,13 @@ module.exports = function(app, passport) {
   var Present = require('./models/present');
 
 
-app.all('/*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-    console.log('in that all function');
-  next();
-});
+// app.all('/*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+//     console.log('in that all function');
+//   next();
+// });
 
 
 
